1d9798042bd6b62dbaba10a3e275233e2db5cce8
Lars Haugseth <lars@larshaugseth.com>
2013-03-11 23:46:18 +0100

    Added more specs.

--- spec/r64/color_spec.rb
+++ spec/r64/color_spec.rb
@@ -2,6 +2,75 @@ require 'r64/color'
 
 describe R64::Color do
 
+  describe "from_symbol" do
+    it "takes lowercase symbols" do
+      subject.from_symbol(:light_red).should eq 10
+    end
+    it "takes uppercase symbols" do
+      subject.from_symbol(:PURPLE).should eq 4
+    end
+  end
+
+  describe "xterm_ansi" do
+    it "maps an index into an ANSI sequence to set foreground color" do
+      subject.xterm_ansi(3).should eq "\033[38;5;6m"
+    end
+    it "maps an index into an ANSI sequence to set background color" do
+      subject.xterm_ansi(7, true).should eq "\033[48;5;185m"
+    end
+  end
+
+  describe "xterm_dump" do
+    it "starts with an ANSI sequence to set background color" do
+      subject.should_receive(:xterm_ansi).with(4, true).once.and_call_original
+      subject.xterm_dump(4)
+    end
+    it "ends with a sequence of spaces" do
+      subject.xterm_dump(4).should end_with("  ")
+      subject.xterm_dump(4, true).should end_with("    ")
+    end
+  end
+
+  describe "from_rgba" do
+    it "delegates to from_rgb without the alpha component" do
+      subject.should_receive(:from_rgb).with(0x123456, nil).once
+      subject.from_rgba(0x12345678)
+      subject.should_receive(:from_rgb).with(0x010203, 123).once
+      subject.from_rgba(0x010203FF, 123)
+    end
+  end
+
+  describe "from_rgb" do
+    it "looks up index in palette and returns it" do
+      subject.should_receive(:palette).once.and_call_original
+      subject.should_not_receive(:guess_from_rgb)
+      subject.from_rgb(0x8F8F8F).should eq 15
+    end
+    it "looks up index in palette and uses passed default if not found" do
+      subject.should_receive(:palette).once.and_call_original
+      subject.should_not_receive(:guess_from_rgb)
+      subject.from_rgb(0x123456, 123).should eq 123
+    end
+    it "looks up index in palette and calls guess_from_rgb if not found" do
+      subject.should_receive(:palette).once.and_call_original
+      subject.should_receive(:guess_from_rgb).with(0x123456).once
+      subject.from_rgb(0x123456)
+    end
+  end
+
+  describe "palette" do
+    it "returns a hash with 24-bit color values as keys" do
+      subject.palette.keys.should include(0xD5D5D5)
+      subject.palette.keys.should include(0x6C5EB5)
+    end
+    it "returns a hash with at least 16 distinct keys" do
+      subject.palette.keys.sort.uniq.size.should be >= 16
+    end
+    it "returns a hash with C64 color indices as values" do
+      subject.palette.values.all? {|v| (0..15).include? v }.should be_true
+    end
+  end
+
   describe "guess_from_rgb" do
     it "correctly guesses the index of all colors in the palette" do
       subject.palette.each do |rgb, index|
