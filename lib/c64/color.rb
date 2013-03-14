7c95bf894a8918f432156b5c82b4382b8e7753fd
Lars Haugseth <lars@larshaugseth.com>
2013-03-14 02:16:24 +0100

    Only add methods to Fixnum and Symbol when submodule is included.

--- lib/c64/color.rb
+++ lib/c64/color.rb
@@ -23,17 +23,19 @@ module C64
     include Names
 
     module Methods
-      class ::Symbol
-        def color
-          C64::Color.from_symbol(self)
+      def self.included(base)
+        Symbol.class_eval do
+          def color
+            C64::Color.from_symbol(self)
+          end
         end
-      end
-      class ::Fixnum
-        def color(default = nil)
-          C64::Color.from_rgb(self, default)
-        end
-        def rgb(palette = :pepto)
-          C64::Color.to_rgb(self, palette)
+        Fixnum.class_eval do
+          def color(default = nil)
+            C64::Color.from_rgb(self, default)
+          end
+          def rgb(palette = :pepto)
+            C64::Color.to_rgb(self, palette)
+          end
         end
       end
     end
