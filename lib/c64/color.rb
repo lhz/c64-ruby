525f69d048bb2b8c3b7fdbe6c2d9e9f2256cbc56
Lars Haugseth <lars@larshaugseth.com>
2013-03-23 17:04:02 +0100

    Added conversion to Rubygame::Color.

--- lib/c64/color.rb
+++ lib/c64/color.rb
@@ -40,6 +40,11 @@ module C64
       end
     end
 
+    def self.[](value)
+      index = (value.is_a?(Symbol) ? from_symbol(value) : value)
+      to_rubygame_color to_rgb(index)
+    end
+
     def self.from_symbol(symbol)
       const_get symbol.to_s.upcase.to_sym
     end
@@ -60,6 +65,13 @@ module C64
       else
         raise "Invalid palette type :#{palette}."
       end
+    end
+
+    def self.to_rubygame_color(rgb_value)
+      r = ((rgb_value & 0xFF0000) >> 16) / 255.0
+      g = ((rgb_value & 0x00FF00) >>  8) / 255.0
+      b = (rgb_value & 0x0000FF) / 255.0
+      Rubygame::Color::ColorRGB.new([r / 255.0, g / 255.0, b / 255.0])
     end
 
     def self.xterm256_escape(index, bg = false)
