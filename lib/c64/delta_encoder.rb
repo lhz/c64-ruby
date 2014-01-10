bae909c244f80962d8ff7ee08353f2209d294c23
Lars Haugseth <github@larshaugseth.com>
2014-01-10 11:42:36 +0100

    Started work on delta encoder for physics animations.

---
+++ lib/c64/delta_encoder.rb
@@ -0,0 +1,38 @@
+module C64
+  class DeltaEncoder
+
+    attr_reader :num_objects
+
+    def initialize(num_objects, num_traits, options = {})
+      @num_objects = num_objects
+      @num_traits  = num_traits
+      @ranges = options[:ranges] || [(-7..7)] * num_traits
+      @steps  = Array.new(num_objects) { [] }
+      @deltas = Hash.new { 0 }
+    end
+
+    # Record the state of an object for a new step
+    def record_step(index, state)
+      step = {s: state}
+      if prev = @steps[index].last
+        step[:d] = prev[:s].map.with_index {|value, i| state[i] - value}
+        @deltas[step[:d]] += 1
+      end
+      @steps[index] << step
+    end
+
+    # Remove deltas with values outside the valid ranges
+    def prune_deltas
+      @deltas.delete_if do |key, value|
+        key.map.with_index.any? {|delta, i| !@ranges[i].include? delta }
+      end
+    end
+
+    def compile
+      prune_deltas
+      puts "STEPS: #{@steps[0].size}"
+      puts "PRUNE: #{@deltas.inspect} (#{@deltas.size})"
+    end
+
+  end
+end
